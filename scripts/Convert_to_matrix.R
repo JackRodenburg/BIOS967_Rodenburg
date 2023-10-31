f89aff0a5ad67dcfe2c515c5107a8b70c7fc393f
dshizuka <dshizuka2@unl.edu>
2023-10-31 13:31:38 -0500

    convert_to_matrix r script

---
+++ scripts/Convert_to_matrix.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+dat=read.csv("data/Platte_Fish_Cleaned.csv")
+head(dat)
+
+dat %>% select(-X, -Length, -Weight) %>%
+  group_by(Segment, Year, Month, Day, Species) %>%
+  summarise(count_use=sum(Count))%>%
+  mutate(sample_id=paste(Segment, Year, Month, Day, sep="-")) %>%
+  pivot_wider(id_cols=sample_id, names_from=Species, values_from=count_use)
+
+
+dat$Count
+dat[482,]
